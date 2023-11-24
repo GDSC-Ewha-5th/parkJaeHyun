b1dd25eb48945d4e314545f5446f8648182487d9
Jaehyun Park <jhjh2002109@ewhain.net>
2023-11-24 11:39:20 +0900

    feat : 모달창(+스크롤제어) 구현

--- seminar4_test/src/App.jsx
+++ seminar4_test/src/App.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faUser, faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Header = styled.header`
   display: flex;
@@ -28,14 +30,24 @@ const MainWrapper = styled.div`
 
 const Logo = styled.h1`
   font-size: 28px;
+  transition: color 0.3s ease; /* 부드러운 전환 효과 */
+  
+  &:hover {
+    color:pink; /* 마우스를 올렸을 때 원하는 색상으로 변경하세요 */
+    /* 또는 마우스를 올렸을 때 원하는 스타일로 변경하세요 */
+  }
 `;
-
 const SearchButton = styled.button`
   font-size: 20px;
   background-color: white;
   border: none;
   cursor: pointer;
   padding : 18px;
+  transition: color 0.3s ease; /* 부드러운 전환 효과 */
+  
+  &:hover {
+    color:pink; 
+  }
 `;
 
 const RightAlignedIcons = styled.div`
@@ -45,6 +57,26 @@ const RightAlignedIcons = styled.div`
   margin-left: 86% ;
 `;
 
+const Modal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+  background-color: white;
+  padding: 20px;
+`;
 
 
 const Title = styled.p`
@@ -59,8 +91,19 @@ const Content = styled.p`
 const Image = styled.img`
   width: 100%; /* 이미지를 포스팅 카드에 맞게 확장 */
   height: auto; /* 비율 유지 */
+  
 `;
 
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 16px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color : black;
+`;
 function App() {
   const posts = [
     {
@@ -152,11 +195,34 @@ function App() {
       title: '프론트엔드 개발자의 끝? V0.dev 사용법',
       content: '프론트엔드 개발자의 일자리를 위협하는 V0의 등장',
       image: '캡처3.PNG',
-    },
-    // 계속해서 원하는 만큼 복사하여 추가해주세요!
-  ];
-  
+    },];
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState('');
 
+  const openModal = (image) => {
+    setModalImage(image);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  useEffect(() => {
+    // 모달이 열렸을 때 body 요소에 overflow: hidden을 적용하여 스크롤을 막음
+    if (showModal) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      // 모달이 닫혔을 때 body 요소의 overflow를 visible로 설정하여 스크롤 복구
+      document.body.style.overflow = 'visible';
+    }
+
+    // 컴포넌트 언마운트 시에도 스크롤 복구 처리
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, [showModal]); // showModal 상태에 변화가 있을 때마다 실행
+
+ 
   return (
     <>
       <Header>
@@ -171,7 +237,7 @@ function App() {
       
       <MainWrapper>
         {posts.map((post) => (
-          <PostingCard key={post.id}>
+          <PostingCard key={post.id} onClick={() => openModal('모달이미지.png')}> {/* 이미지 클릭 시 모달 열기 */}
             <Image src={post.image} alt="post" />
             <div>
               <Title>{post.title}</Title>
@@ -179,7 +245,15 @@ function App() {
             </div>
           </PostingCard>
         ))}
-         </MainWrapper>
+      </MainWrapper>
+      {showModal && (
+    <Modal>
+    <ModalContent>
+      <img src={modalImage} alt="modal" />
+      <CloseButton onClick={closeModal}>X</CloseButton> {/* X 버튼 추가 */}
+    </ModalContent>
+  </Modal>
+      )}
       
     </>
   );
